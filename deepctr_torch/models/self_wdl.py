07d54dd18f14770b2cc27bf067d6d68483a59bd5
Miracleyin <miracleyin@live.com>
2021-03-18 21:07:36 +0800

    torch run_wdl

--- deepctr_torch/models/self_wdl.py
+++ deepctr_torch/models/self_wdl.py
@@ -6,14 +6,67 @@
 # Reference:
 #    [1] Cheng H T, Koc L, Harmsen J, et al. Wide & deep learning for recommender systems[C]//Proceedings of the 1st Workshop on Deep Learning for Recommender Systems. ACM, 2016: 7-10.(https://arxiv.org/pdf/1606.07792.pdf)
 import torch.nn as nn
+import torch.nn.functional as F
+import torch
+#from .basemodel import BaseModel
+#from ..inputs import combined_dnn_input
+#from ..layers import DNN
+class Linear(nn.Module):
+    def __init__(self, input_dim):
+        super(Linear, self).__init__()
+        self.linear = nn.Linear(in_features=input_dim, out_features=1)
+    def forward(self, X):
+        return self.linear(X)
 
-from .basemodel import BaseModel
-from ..inputs import combined_dnn_input
-from ..layers import DNN
+class DNN(nn.Module):
+    def __init__(self, hidden_units, dropout=0.):
+        super(DNN, self).__init__()
+        self.dnn_network = nn.ModuleList(
+            [nn.Linear(layer[0], layer[1]) for layer in list(zip(hidden_units[:-1], hidden_units[1:]))]
+        )
+        self.dropout = nn.Dropout(p=dropout)
+    def forward(self, X):
+        for linear in self.dnn_network:
+            X = linear(X)
+            X = F.relu(X)
 
-class self_WDL(BaseModel):
+        X = self.dropout(X)
+        return X
+class self_WDL(nn.Module):
+    def __init__(self, feature_colums, hidden_units, dnn_dropout=0.):
+        super(self_WDL, self).__init__()
+        self.dense_feature_cols, self.sparse_feature_cols = feature_colums
+        # embedding
+        self.embed_layers = nn.ModuleDict({
+            'embed_' + str(i): nn.Embedding(num_embeddings=feat['feat_num'], embedding_dim=feat['embed_dim'])
+            for i, feat in enumerate(self.sparse_feature_cols)
+        })
+
+        hidden_units.insert(0, len(self.dense_feature_cols) + len(self.sparse_feature_cols)*self.sparse_feature_cols[0]['embed_dim'])
+        self.dnn_network = DNN(hidden_units)
+        self.linear = Linear(len(self.dense_feature_cols))
+        self.final_linear = nn.Linear(hidden_units[-1], 1)
+
+    def forward(self, X):
+        dense_input, sparse_inputs = X[:, :len(self.dense_feature_cols)], X[:, len(self.dense_feature_cols):]
+        sparse_inputs = sparse_inputs.long()
+        sparse_embeds = [self.embed_layers['embed_' + str(i)](sparse_inputs[:, i]) for i in
+                         range(sparse_inputs.shape[1])]
+        sparse_embeds = torch.cat(sparse_embeds, axis=-1)
+        dnn_input = torch.cat([sparse_embeds, dense_input], axis=-1)
+        # Wide
+        wide_out = self.linear(dense_input)
+
+        # Deep
+        deep_out = self.dnn_network(dnn_input)
+        deep_out = self.final_linear(deep_out)
+
+        # out
+        outputs = torch.sigmoid(0.5 * (wide_out + deep_out))
+
+        return outputs
+    #class self_WDL(nn.Module):
     """Instantiates the Wide&Deep Learning architecture.
-
     :param linear_feature_columns: 包含所有用于线性模型部分的迭代器
     :param dnn_feature_columns:    包含所有用于深度网络部分的迭代器
     :param dnn_hidden_units:       一个包含正整数或为空的列表，为深度神经网络隐藏层的层数和宽度
@@ -29,39 +82,7 @@ class self_WDL(BaseModel):
     :return: A PyTorch model instance.
 
     """
-    def __init__(self,
-                 linear_feature_columns, dnn_feature_columns, dnn_hidden_units=(256,128),
-                 l2_reg_linear=1e-5,
-                 l2_reg_embedding=1e-5, l2_reg_dnn=0, init_std=0.0001, seed=1024, dnn_dropout=0, dnn_activation='relu',
-                 dnn_use_bn=False,
-                 task='binary', device='cpu'):
-        super(self_WDL, self).__init__(linear_feature_columns, dnn_feature_columns, l2_reg_linear=l2_reg_linear,
-                                       l2_reg_embedding=l2_reg_embedding, init_std=init_std, seed=seed, task=task,
-                                       device=device)
-        self.use_dnn = len(dnn_feature_columns) > 0 and len(
-            dnn_hidden_units) > 0 # 如果深度网络特征和深度网络隐藏层都存在
-        if self.use_dnn:
-            self.dnn = DNN(self.compute_input_dim(dnn_feature_columns), dnn_hidden_units,
-                           activation=dnn_activation, l2_reg=l2_reg_dnn, dropout_rate=dnn_dropout, use_bn=dnn_use_bn,
-                           init_std=init_std, device=device)
-            self.dnn_linear = nn.Linear(dnn_hidden_units[-1], 1, bias=False).to(device)
-            self.add_regularization_weight(
-                filter(lambda x: 'weight' in x[0] and 'bn' not in x[0], self.dnn.named_parameters()), l2=l2_reg_dnn)
-            self.add_regularization_weight(self.dnn_linear.weight, l2=l2_reg_dnn)
-        self.to(device)
 
-    def forward(self, X):
-        sparse_embedding_list, dense_value_list = self.input_from_feature_columns(X, self.dnn_feature_columns,
-                                                                                  self.embedding_dict)
-        logit = self.linear_model(X)
-
-        if self.use_dnn:
-            dnn_input = combined_dnn_input(sparse_embedding_list, dense_value_list)
-            dnn_output = self.dnn(dnn_input)
-            dnn_logit = self.dnn_linear(dnn_output)
-            logit += dnn_logit
-        y_pred = self.out(logit)
-
-        return y_pred
-
-
+if __name__ == "__main__":
+    net = self_WDL([256,128,64,32,16,8])
+    print(net)
